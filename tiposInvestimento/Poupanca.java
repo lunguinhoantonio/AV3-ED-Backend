f9204e92705fca985d85e15a77be93c930cd6440
lunguinhoantonio <162999553+lunguinhoantonio@users.noreply.github.com>
2025-05-08 22:10:12 -0300

    feat(tiposInvestimento): update Poupanca
    feat(tiposInvestimento): change Poupanca to store rentability in porcRendimento, update calcValorFinal, and override toString() to include additional information

--- tiposInvestimento/Poupanca.java
+++ tiposInvestimento/Poupanca.java
@@ -7,16 +7,23 @@ public class Poupanca extends InvestimentoMensal {
     }
 
     @Override
-    public double calcRentabilidade() {
-        return ((getMontante() - getCapital()) / getCapital()) * 100;
+    public void calcRentabilidade() {
+        setPorcRendimento(((getMontante() - getCapital()) / getCapital()) * 100);
     }
 
     @Override
     public void calcValorFinal() {
         setMontante(getCapital() * Math.pow(1 + getTaxaPoupanca(), getQuantMeses()));
+        calcRentabilidade();
     }
 
     public double getTaxaPoupanca() {
         return 0.006743;
+    }
+
+    @Override
+    public String toString() {
+        return "Tipo do Investimento: Poupança, " + super.toString() +
+                ", Taxa Poupança: " + getTaxaPoupanca() * 100 + "%";
     }
 }

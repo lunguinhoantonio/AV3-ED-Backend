45d629dcccbd01d3a4d0c8605d72321aa3f445db
lunguinhoantonio <162999553+lunguinhoantonio@users.noreply.github.com>
2025-05-09 21:14:32 -0300

    refactor(TesouroSelic): use getTempoInvestido instead of getQuantAnos in calcValorFinal

--- tiposInvestimento/TesouroSelic.java
+++ tiposInvestimento/TesouroSelic.java
@@ -12,7 +12,7 @@ public class TesouroSelic extends InvestimentoAnual {
 
     @Override
     public void calcValorFinal() {
-        setMontante(getCapital() * Math.pow(1 + getTaxaSelic(), getQuantAnos()));
+        setMontante(getCapital() * Math.pow(1 + getTaxaSelic(), getTempoInvestido()));
         calcRentabilidade();
     }
 
@@ -22,7 +22,7 @@ public class TesouroSelic extends InvestimentoAnual {
 
     @Override
     public String toString() {
-        return "ID: " + getId() + " Tipo do Investimento: Tesouro Selic" + super.toString() +
+        return "ID: " + getId() + ", Tipo do Investimento: Tesouro Selic" + super.toString() +
                 ", Taxa Selic: " + getTaxaSelic() * 100 + "%";
     }
 }
